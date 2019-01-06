a4439cf34d423e8d98575545b5cdb8fc78f04cf0
Andres flors <fmandres92@gmail.com>
2019-01-06 10:35:41 -0400

    login

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -11,7 +11,8 @@ import { Router } from '@angular/router';
 import { JwtHelper } from './services/jwt-helper.service';
 import { UserSession, UserLogin } from './models/user-session';
 import { SessionKeys } from '../shared/local-store/sessionKeys';
-import { Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-login',
@@ -20,110 +21,165 @@ import { Subscription } from 'rxjs';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit, OnDestroy {
-
-    submitted = false;
+    /**
+     * @property {boolean} submitted Flag que indica que el formulario fue enviado
+     */
+    public submitted: boolean;
+    /**
+     * @property {string} title Almacena el titulo
+     */
     public title: string;
+    /**
+     * @property {string} content Almacena el contenido
+     */
     public content: string;
-    suscription:  Subscription;
-    loginForm: FormGroup;
-    userNameCtrl: FormControl;
-    passwordCtrl: FormControl;
-
-    constructor(private formBuilder: FormBuilder,
-                public sercoAuthService: SercoAuthService,
-                public localStorage: LocalStoreManager,
-                private router: Router,
-                private spinner: NgxSpinnerService,
-                private modalService: BsModalService) {
-        this.cleanSession();
-    }
-
-    @ViewChild('messageModal') modalTemplate: TemplateRef<any>;
+    /**
+     * @property {Subject<void>} destroySubject Observable para eliminar las suscripciones
+    */
+    private destroySubject$: Subject<void> = new Subject();
+    /**
+     * @property {FormGroup} loginForm Iniciar instancia del formulario
+    */
+    public loginForm: FormGroup;
+    /**
+     * @property {string} unamePattern Patron para validar que el email sea valido
+     */
+    private unamePattern: string;
+    /**
+     * @property {BsModalRef} bsModalRef Modal referencial
+     */
     public bsModalRef: BsModalRef;
+    /**
+     * @property {TemplateRef} modalTemplate Template referencia
+     */
+    @ViewChild('messageModal')
+    public modalTemplate: TemplateRef<any>;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        public sercoAuthService: SercoAuthService,
+        public localStorage: LocalStoreManager,
+        private router: Router,
+        private spinner: NgxSpinnerService,
+        private modalService: BsModalService
+        ) {
+            this.cleanSession();
+            this.unamePattern = '^[a-zA-Z0-9]*$';
+        }
 
     ngOnInit() {
+        this.initForm();
+        this.validateStorage();
+    }
+
+    /**
+     * Metodo para iniciar el formulario
+     * @method initForm
+     */
+    private initForm(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: [null, [ Validators.required, Validators.pattern(this.unamePattern), Validators.maxLength(15) ]],
+            password: [null, [ Validators.required]],
+        });
+    }
+
+    /**
+     * Metodo para validar que el navegador soporta localstore
+     * @method validateStorage
+     */
+    private validateStorage(): void {
         if (typeof(Storage) === 'undefined') {
             throw new Error('LocalStorage no soportado en este navegador');
         }
-
-        const unamePattern = '^[a-zA-Z0-9]*$';
-        this.loginForm = this.formBuilder
-                             .group({
-                                     userName: this.userNameCtrl,
-                                     password: ['', [
-                                                     Validators.required
-                                                 ]]});
-        this.userNameCtrl = this.formBuilder.control('', {
-            validators: [Validators.required, Validators.pattern(unamePattern),  Validators.maxLength(15)]
-        });
-
-        this.passwordCtrl = this.formBuilder.control('', {
-            validators: [Validators.required]
-        });
     }
 
-    onSubmit() {
+    /**
+     * Metodo que captura el submit del formulario
+     * @method onSubmit
+     */
+    public onSubmit(): void {
         this.submitted = true;
-        this.spinner.show();
-        if (this.passwordCtrl.valid && this.userNameCtrl.valid) {
+        if (this.loginForm.valid) {
+            this.spinner.show();
 
             const userAuth = new UserLogin(
-                this.userNameCtrl.value,
-                this.passwordCtrl.value
+                this.loginForm.controls.userName.value,
+                this.loginForm.controls.passwordCtrl.value,
             );
 
-            this.suscription = this.sercoAuthService
-                                   .login(userAuth)
-                                   .subscribe((Response: AuthBearer) => {
-                                                  this.processLoginResponse(Response);
-                                               },
-                                               error => {
-                                                  this.spinner.hide();
-                                                  console.error(error);
-                                               });
+            this.sercoAuthService.login(userAuth).pipe(takeUntil(this.destroySubject$)).subscribe((Response: AuthBearer) => {
+                this.processLoginResponse(Response);
+            },
+            error => {
+                this.handleError(error);
+            });
         }
     }
 
-    private processLoginResponse(Response: AuthBearer) {
-       console.log(Response);
-       if (typeof Response === 'undefined' || Response.state !== 1) {
-           this.spinner.hide();
-           const message = `<p class="txt-bold">${Response.message}</p>`;
-           this.openModal(message, 'Signar - Aviso');
-           return;
+    /**
+     * Metodo para manajera los errores de la respuesta
+     * @param {err}
+     * @method handleError
+     */
+    private handleError(err): void {
+        this.spinner.hide();
+        this.openModal(err.message);
+    }
+
+    /**
+     * Metodo que maneja la respuesta de la peticion al servicio
+     * @param {AuthBearer} response
+     * @method processLoginResponse
+     */
+    private processLoginResponse(response: AuthBearer) {
+        this.spinner.hide();
+       if (typeof response === 'undefined' || response.state !== 1) {
+           this.handleUndefinedResponse(response);
        }
-       if (Response.state && Response.state === 1 && Response.data && Response.data.accessToken) {
-           const accessToken = Response.data.accessToken;
+       if (response.state && response.state === 1 && response.data && response.data.accessToken) {
+           this.handleSuccesResponse(response);
+       } else {
+           this.handleError(response);
+       }
+    }
+
+    /**
+     * Metodo para manejar la respuesta undefined del servicio
+     * @param {AuthBearer} data
+     * @method handleUndefinedResponse
+     */
+    private handleUndefinedResponse(response: AuthBearer): void {
+        const message = `<p class="txt-bold">${response.message}</p>`;
+        this.openModal(message, 'Signar - Aviso');
+   }
+
+    /**
+     * Metodo para manejar la respuesta exitosa del servicio
+     * @param {AuthBearer} data
+     * @method handleUndefinedResponse
+     */
+    private handleSuccesResponse(response: AuthBearer): void {
+        const accessToken = response.data.accessToken;
            const jwtHelper = new JwtHelper();
-           const decodeToken = jwtHelper.decodeToken(Response.data.accessToken);
-           const expiresIn = decodeToken.exp;
+           const decodeToken = jwtHelper.decodeToken(response.data.accessToken);
            const userSession = new UserSession(
                decodeToken.jti,
                decodeToken.unique_name,
                decodeToken.c_hash,
            );
-           this.saveUserSession(userSession, accessToken, expiresIn);
+           this.saveUserSession(userSession, accessToken);
            this.router.navigate(['/home']);
-       } else {
-           this.spinner.hide();
-           this.openModal(Response.message);
-           console.error('c% processLoginResponse:', 'color: red', Response.MessageException);
-       }
+   }
+
+    public openModal(content: string, title: string = 'Signar'): void {
+        const initialState = { title, content };
+        this.modalService.show(this.modalTemplate, {
+                                                    initialState,
+                                                    class: 'gray modal-md'
+                                                });
     }
 
-    openModal(message: string, title: string = 'Signar') {
-        this.content = message;
-        this.title = title;
-        const initialState = {
-            title: message,
-            content: this.content
-        };
-        this.bsModalRef = this.modalService.show(this.modalTemplate, {
-                                                                      initialState,
-                                                                      class: 'gray modal-md'});
-    }
-
-    private saveUserSession(userSession: UserSession, accessToken: string, expiresIn: Date) {
+    private saveUserSession(userSession: UserSession, accessToken: string): void {
         this.localStorage.sessionStorageSetItem(SessionKeys.ACCESS_TOKEN, accessToken);
         this.localStorage.sessionStorageSetItem(SessionKeys.CURRENT_USER, userSession);
     }
@@ -133,8 +189,8 @@ export class LoginComponent implements OnInit, OnDestroy {
         localStorage.clear();
     }
 
-    ngOnDestroy() {
-        this.suscription.unsubscribe();
+    ngOnDestroy(): void {
+        this.destroySubject$.next();
     }
 }
 
